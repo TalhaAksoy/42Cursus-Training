67c258b4508836255163518f4a16bf12c0fd0e90
Talha Aksoy <56833887+TalhaAksoy@users.noreply.github.com>
2022-02-25 18:21:28 +0300

    Update printf.c

--- exam-02/printf.c
+++ exam-02/printf.c
@@ -15,63 +15,69 @@
 
 int ft_putstr(char *str)
 {
-	int i = 0;
-	int len = 0;
-	while(str[i])
-	{
-		len += write(1, &str[i], 1);
-		i++;
-	}
-	return (len);
+    int i = 0;
+    int len = 0;
+    while(str[i])
+    {
+        len += write(1, &str[i], 1);
+        i++;
+    }
+    return (len);
 }
 
-int ft_setprint(unsigned long number, char *set, int numset)
+int ft_putnbr(long int number)
 {
-	int i = 0;
-	int len = 0;
-	if(number < 0)
-	{
-		len += write(1, "-", 1);
-		number = number * -1;
-	}
-	if(number > numset - 1)
-		len += ft_setprint(number/numset,set,numset);
-	len += write(1, &set[number%numset], 1);
-	return (len);
+    int len = 0;
+    if(number == -2147483648)
+        return(ft_putstr("-2147483648"));
+    if(number < 0)
+    {
+        len += write(1, "-", 1);
+        number = number * -1;
+    }
+    if(number > 9)
+        len += ft_putnbr(number/10);
+    len += write(1, &"0123456789"[number%10], 1);
+    return (len);
+}
+
+int ft_puthex(long int number)
+{
+    if(number > 15)
+        len += ft_puthex(number/16);
+    len += write(1, &"0123456789abcdef"[number%16], 1);
+    return (len);
 }
 
 int arg_printer(va_list macro, char c)
 {
-	if(c == 's')
-		return(ft_putstr(va_arg(macro, char *)));
-	if(c == 'd')
-		return(ft_setprint(va_arg(macro, int), "0123456789", 10));
-	if(c == 'x')
-		return(ft_setprint(va_arg(macro, unsigned int), "0123456789abcdef", 16));
-	return (0);
-}
-int ft_printf(const char *str, ...)
-{
-	va_list macro;
-	int i = 0;
-	int len = 0;
-
-	va_start(macro, str);
-	while(str[i])
-	{
-		if(str[i] == '%')
-		{
-			len += arg_printer(macro, str[i + 1]);
-			i++;
-		}
-		else
-			len += write(1, &str[i], 1);
-		i++;
-	}
-	return (len);
+    if(c == 's')
+        return (ft_putstr(va_arg(macro, char *)));
+    if(c == 'd')
+        return (ft_putnbr(va_arg(macro, int));
+    if(c == 'x')
+        return (ft_puthex(va_arg(macro, unsigned int));
+    return (0);
 }
 
-int main()
+int ft_printf(char *str, ...)
 {
-	ft_printf("%x", -42);
+    va_list macro;
+    int i = 0;
+    int len = 0;
+    
+    va_start(macro, str);
+    while(str[i])
+    {
+        if(str[i] == '%')
+        {
+            len += arg_printer(macro, str[i + 1]);
+            i++;
+        }
+        else
+            len += write(1, &str[i], 1);
+        i++;
+    }
+    va_end(macro);
+    return (len);
 }
